59a0eff3142eaa910f1e1ae95ed9aebae20a36b2
Atheer Abdullatif <42766508+athlatif@users.noreply.github.com>
2021-03-22 17:51:58 +0300

    add comments

--- modelConsumer.py
+++ modelConsumer.py
@@ -2,21 +2,21 @@ from kafka import KafkaConsumer
 import json
 
 
-
+# sudo AI Engine function
 def passTomodel(product_number,userID):
-    
-    
     
     output = ['#156','#1768']
     return output
 
+# init consumer with group ID
 consumer = KafkaConsumer('heartbeatlogs',
                          group_id='CConsumerlogs',
                          bootstrap_servers=['localhost:9092'],
                          enable_auto_commit=True,
                          value_deserializer=lambda m: json.loads(m.decode('ascii')))
+# loop over events 
 for message in consumer:
-
     print(message.value)
+    # send only product page logs
     if(message.value['page'] == 'product page'):
         print(passTomodel(message.value['product_number'], message.value['userID']))
